3ed110bb027b0ffc867470e2f0b594b2b4f69e91
Abdullahi Yusuf <45642669+lovowAb@users.noreply.github.com>
2021-10-31 20:56:18 +0200

    operators

---
+++ addition.java
@@ -0,0 +1,27 @@
+package operators;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//integer 1 2 3 4 5 6 7 8 9
+		int x = 15;
+		int y = 10;
+		int sum = x + y;
+		System.out.println("The sum of x and y = " + sum);
+		
+		//double 0.1 0.2 0.3 0.4 0.5
+		double c = 4.5;
+		double r = 3.6;
+		double sum1 = c * r;
+		System.out.println("The multiplication of c and r = " + sum1);
+		
+		//float 
+		float e = 25;
+		float w = 10;
+		float sum2 = e / w;
+		System.out.println("The division of e and w = " + sum2);
+	}
+
+}
