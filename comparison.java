63e1be5d4cc87a53d8e3ec0239830b64bfbe9787
Abdullahi Yusuf <45642669+lovowAb@users.noreply.github.com>
2021-12-15 01:48:21 +0200

    Add files via upload

---
+++ comparison.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class comparison {
+
+	public static void main(String[] args) {
+		
+		// Comparison Operators
+		int ahmedAge = 20;
+		int mohamedAge = 15;
+		// greater than
+		System.out.println(ahmedAge > mohamedAge);
+		// less than
+		System.out.println(ahmedAge < mohamedAge);
+		// equal
+		System.out.println(ahmedAge == mohamedAge);
+		// not equal
+		System.out.println(ahmedAge != mohamedAge);
+		// greater than or equal
+		System.out.println(ahmedAge >= mohamedAge);
+		// less than or equal
+		System.out.println(ahmedAge <= mohamedAge);
+	}
+}
